e0df7b3f27b92c0d2a3aa8fdcfcd920dba48ccdb
ICB Anna Schaar <anna.schaar@icb-gpusrv06.scidom.de>
2022-01-25 13:30:26 +0100

    resolve conflict

--- ncem/data.py
+++ ncem/data.py
@@ -3065,7 +3065,7 @@ class DataLoaderLuWT(DataLoader):
             X=celldata_df[feature_cols],
             obs=celldata_df[["CellID", "FOV", "CellTypeID_new", "Center_x", "Center_y"]]
         )
-
+        
         celldata.uns["metadata"] = metadata
         img_keys = list(np.unique(celldata_df[metadata["image_col"]]))
         celldata.uns["img_keys"] = img_keys
@@ -3092,7 +3092,6 @@ class DataLoaderLuWT(DataLoader):
 
         # register node type names
         node_type_names = list(np.unique(celldata.obs[metadata["cluster_col_preprocessed"]]))
-        print(node_type_names)
         celldata.uns["node_type_names"] = {x: x for x in node_type_names}
         node_types = np.zeros((celldata.shape[0], len(node_type_names)))
         node_type_idx = np.array(
@@ -3398,14 +3397,13 @@ class DataLoaderLuTET2(DataLoader):
 
         # add clean cluster column which removes regular expression from cluster_col
         celldata.obs[metadata["cluster_col_preprocessed"]] = list(
-            pd.Series(list(celldata.obs[metadata["cluster_col"]]), dtype="category").map(self.cell_type_merge_dict)
+            pd.Series(list(celldata.obs[metadata["cluster_col"]]), dtype="str").map(self.cell_type_merge_dict)
         )
         celldata.obs[metadata["cluster_col_preprocessed"]] = celldata.obs[metadata["cluster_col_preprocessed"]].astype(
-            "category"
+            "str"
         )
         # register node type names
         node_type_names = list(np.unique(celldata.obs[metadata["cluster_col_preprocessed"]]))
-        print(node_type_names)
         celldata.uns["node_type_names"] = {x: x for x in node_type_names}
         node_types = np.zeros((celldata.shape[0], len(node_type_names)))
         node_type_idx = np.array(
