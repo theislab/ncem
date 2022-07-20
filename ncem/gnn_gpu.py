3917f39f224d77785b0e4a88e81d895aa8ca35fd
syelman <syozleyen@gmail.com>
2022-07-20 19:25:55 +0200

    training and validation implemented

--- ncem/gnn_gpu.py
+++ ncem/gnn_gpu.py
@@ -13,37 +13,35 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 import torch.optim as optim
 import os
+from sklearn.metrics import r2_score
 
 class GNNModel(nn.Module):
 
-    def __init__(self, c_in=11, c_hidden=34, c_out=34, num_layers=2, layer_name="GCN", dp_rate=0.1, **kwargs):
+    def __init__(self, in_channels=11, hidden_dims=None, out_channels=34, **kwargs):
         """
         Inputs:
-            c_in - Dimension of input features
-            c_hidden - Dimension of hidden features
-            c_out - Dimension of the output features. Usually number of classes in classification
-            num_layers - Number of "hidden" graph layers
-            layer_name - String of the graph layer to use
-            dp_rate - Dropout rate to apply throughout the network
-            kwargs - Additional arguments for the graph layer (e.g. number of heads for GAT)
+            in_channels - Dimension of input features
+            out_channels - Dimension of the output features.
+            hidden_dims - List of hidden dimensions
         """
         super().__init__()
-        gnn_layer = geom_nn.GCNConv
 
         layers = []
-        in_channels, out_channels = c_in, c_hidden
-        for l_idx in range(num_layers-1):
-            layers += [
-                gnn_layer(in_channels=in_channels,
-                          out_channels=out_channels,
-                          **kwargs),
-                nn.ReLU(inplace=True),
-                nn.Softmax()
-            ]
-            in_channels = c_hidden
-        layers += [gnn_layer(in_channels=in_channels,
-                             out_channels=c_out,
-                             **kwargs)]
+        prev_dim = in_channels
+        if hidden_dims is not None:
+            for dim in hidden_dims:
+                layers += [
+                    geom_nn.GCNConv(
+                        in_channels=prev_dim,
+                        out_channels=dim),
+                    nn.ReLU(inplace=True),
+                    nn.Softmax()
+                ]
+                prev_dim = dim
+
+        layers += [geom_nn.GCNConv(in_channels=prev_dim,
+                                   out_channels=out_channels)]
+
         self.layers = nn.ModuleList(layers)
 
     def forward(self, x, edge_index):
@@ -62,30 +60,33 @@ class GNNModel(nn.Module):
                 x = l(x)
         return x
 
+
 class MLPModel(nn.Module):
 
-    def __init__(self, c_in=34, c_hidden=34, c_out=34, num_layers=1, dp_rate=0.1):
+    def __init__(self, in_channels=34, hidden_dims=None, out_channels=34, dp_rate=0.1):
         """
         Inputs:
-            c_in - Dimension of input features
-            c_hidden - Dimension of hidden features
-            c_out - Dimension of the output features. Usually number of classes in classification
-            num_layers - Number of hidden layers
+            in_channels - Dimension of input features
+            out_channels - Dimension of the output features.
+            hidden_dims - List of hidden dimensions
             dp_rate - Dropout rate to apply throughout the network
         """
         super().__init__()
-        #layers = []
-        in_channels, out_channels = c_in, c_out
-        '''for l_idx in range(num_layers-1):
-            layers += [
-                nn.Linear(in_channels, out_channels),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dp_rate)
-            ]
-            in_channels = c_hidden
-        layers += [nn.Linear(in_channels, c_out)]'''
-        self.layers = nn.Sequential(nn.Linear(34, 34),
-                nn.ReLU(inplace=True))
+        layers = []
+
+        prev_dim = in_channels
+        if hidden_dims is not None:
+            for dim in hidden_dims:
+                layers += [
+                    nn.Linear(prev_dim, dim),
+                    nn.ReLU(inplace=True),
+                    nn.Dropout(dp_rate)
+                ]
+                prev_dim = dim
+
+        layers += [nn.Linear(prev_dim, out_channels), nn.ReLU(inplace=True)]
+
+        self.layers = nn.Sequential(*layers)
 
     def forward(self, x, *args, **kwargs):
         """
@@ -94,112 +95,87 @@ class MLPModel(nn.Module):
         """
         return self.layers(x)
 
-class NodeLevelGNN(pl.LightningModule):
 
-    def __init__(self, model_name, **model_kwargs):
+class NonLinearNCEM(pl.LightningModule):
+    def __init__(self, **model_kwargs):
         super().__init__()
         # Saving hyperparameters
-        self.save_hyperparameters()
+        self.save_hyperparameters(model_kwargs)
 
-        if model_name=="nonlinear NCEM":
-            encoder= GNNModel(**model_kwargs)
-            decoder= MLPModel(**model_kwargs)
+        self.encoder = GNNModel(
+            in_channels=self.hparams.in_channels,
+            hidden_dims=self.hparams.encoder_hidden_dims,
+            out_channels=self.hparams.latent_dim,
+        )
 
-        self.encoder = encoder
-        self.decoder = decoder
-        self.loss_module = nn.GaussianNLLLoss()
+        self.decoder = MLPModel(
+            in_channels=self.hparams.latent_dim,
+            out_channels=self.hparams.out_channels * 2,  # one for means one for vars
+            hidden_dims=self.hparams.decoder_hidden_dims,
+        )
 
-    def forward(self, data, mode="train"):
+        self.loss_module = nn.GaussianNLLLoss(eps=1e-5)
+
+        def init_weights(m):
+            if isinstance(m, geom_nn.GCNConv): 
+                # TODO: how to init weights of GNN's?
+                pass
+                # torch.nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+                # m.bias.data.fill_(0.01)
+            elif isinstance(m, nn.Linear):
+                torch.nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+                m.bias.data.fill_(0.01)
+
+        self.encoder.apply(init_weights)
+        self.decoder.apply(init_weights)
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group("NonLinearNCEM")
+        parser.add_argument("--lr", type=float, default=0.1, help="the initial learning rate")
+        parser.add_argument("--momentum", type=float, default=0.9, help="momentum")
+        parser.add_argument("--weight-decay", type=float, default=2e-3, help="the weight decay")
+        parser.add_argument("--latent_dim", type=int, default=30, help="the weight decay")
+        parser.add_argument("--decoder_hidden_dims", action='append',
+                            default=None, help="Decoder Hidden Layer Dim")  # TODO Test
+        parser.add_argument("--encoder_hidden_dims", action='append',
+                            default=None, help="Encoder Hidden Layer Dim")  # TODO Test
+        parser.add_argument("--dp_rate", type=float, default=0.1, help="Dropout Rate")
+        return parent_parser
+
+    def forward(self, data):
         x, edge_index = data.x, data.edge_index
         x = self.encoder(x, edge_index)
         x = self.decoder(x)
-
-        ''' # Only calculate the loss on the nodes corresponding to the mask
-        if mode == "train":
-            mask = data.train_mask
-        elif mode == "val":
-            mask = data.val_mask
-        elif mode == "test":
-            mask = data.test_mask
-        else:
-            assert False, f"Unknown forward mode: {mode}"'''
-
-
-        var=torch.ones(x.shape[0], x.shape[1])
-        loss = self.loss_module(x, data.y, var)
-        return loss
+        latent_dim = x.shape[-1]
+        assert latent_dim == self.hparams.out_channels * 2
+        mu, sigma = x[:, :latent_dim // 2], x[:, latent_dim // 2:]
+        sigma = sigma + 1 # for numeric stability, TODO: is this OK?
+        return mu, sigma
 
     def configure_optimizers(self):
         # We use SGD here, but Adam works as well
-        optimizer = optim.SGD(self.parameters(), lr=0.1, momentum=0.9, weight_decay=2e-3)
+        optimizer = optim.SGD(
+            self.parameters(),
+            lr=self.hparams["lr"],
+            momentum=self.hparams["momentum"],
+            weight_decay=self.hparams["weight_decay"],
+        )
         return optimizer
 
-    def training_step(self, batch, batch_idx):
-        loss = self.forward(batch, mode="train")
-        self.log('train_loss', loss)
+    def training_step(self, batch, _):
+        mu, sigma = self.forward(batch)
+        loss = self.loss_module(mu, batch.y, sigma)
+        self.log('train_loss', loss, batch_size=batch.batch_size)
         return loss
 
-    def validation_step(self, batch, batch_idx):
-        loss= self.forward(batch, mode="val")
-        self.log('val_loss', loss)
+    def validation_step(self, batch, _):
+        mu, sigma = self.forward(batch)
+        val_loss = self.loss_module(mu, batch.y, sigma)
+        val_r2_score = r2_score(batch.y, mu)
+        self.log('val_r2_score', val_r2_score, batch_size=batch.batch_size, prog_bar=True)
+        self.log('val_loss', val_loss, batch_size=batch.batch_size, prog_bar=True)
 
-    def test_step(self, batch, batch_idx):
-        loss = self.forward(batch, mode="test")
-        self.log('test_loss', loss)
-
-def train_node_classifier(cur_dir, model_name, data, **model_kwargs):
-    pl.seed_everything(42)
-    node_data_loader = LightningNodeData(data, data.train_mask, loader='neighbor',
-                                   num_neighbors=[30] * 2, batch_size=128,
-                                   num_workers=8)
-    strategy = pl.plugins.DDPSpawnPlugin(find_unused_parameters=False)
-    '''NeighborLoader(
-    data,
-    # Sample 30 neighbors for each node for 2 iterations
-    num_neighbors=[30] * 2,
-    # Use a batch size of 128 for sampling training nodes
-    batch_size=128)'''
-
-    CHECKPOINT_PATH = cur_dir + "/checkpoints/"
-
-    # Create a PyTorch Lightning trainer with the generation callback
-    root_dir = os.path.join(CHECKPOINT_PATH, model_name)
-    os.makedirs(root_dir, exist_ok=True)
-    trainer = pl.Trainer(strategy=strategy, default_root_dir=root_dir, callbacks=[ModelCheckpoint(save_weights_only=True, mode="min", monitor="val_loss")],
-                         min_epochs=20, max_epochs=20,
-                         progress_bar_refresh_rate=0) # 0 because epoch size is 1
-
-    
-    model = NodeLevelGNN(model_name=model_name, **model_kwargs)
-    trainer.fit(model, node_data_loader)
-
-    '''# Check whether pretrained model exists. If yes, load it and skip training
-    pretrained_filename = os.path.join(CHECKPOINT_PATH, f"{model_name}.ckpt")
-    if os.path.isfile(pretrained_filename):
-        print("Found pretrained model, loading...")
-        model = NodeLevelGNN.load_from_checkpoint(pretrained_filename)
-    else:
-        pl.seed_everything()
-        model = NodeLevelGNN(model_name=model_name, **model_kwargs)
-        trainer.fit(model, node_data_loader)
-        #model = NodeLevelGNN.load_from_checkpoint(trainer.checkpoint_callback.best_model_path)
-        '''
-    # Test best model on the test set
-    
-    val_result = trainer.validate(model, node_data_loader, verbose=False)
-    '''result = {"train": train_loss,
-              "val": val_loss,
-              "test": test_result['test_loss']}'''
-    result=val_result
-    return model, result
-
-def print_results(result_dict):
-    if "train" in result_dict:
-        print(f"Train loss: {result_dict['train']}")
-    if "val" in result_dict:
-        print(f"Val loss:   {result_dict['val']}")
-    print(f"Test loss:  {result_dict['test']}")
-
-
-
-
+    def test_step(self, batch, _):
+        mu, sigma = self.forward(batch)
+        self.log('test_loss', loss, batch_size=batch.batch_size)
