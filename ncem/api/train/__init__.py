1b8342b7cd20aeb1a22f91af0b89556b59fee04e
Anna Schaar <anna.schaar@tum.de>
2022-08-08 14:53:50 +0200

    check doc building error

--- ncem/api/train/__init__.py
+++ ncem/api/train/__init__.py
@@ -1,4 +1,5 @@
 """Initializes a train object in api."""
+import numpy as np
 from ncem.estimators import (Estimator, EstimatorCVAE, EstimatorCVAEncem,
                              EstimatorED, EstimatorEDncem, EstimatorEdNcemNeighborhood, EstimatorGraph,
                              EstimatorInteractions, EstimatorLinear,
