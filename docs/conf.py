d8dc90c2d2515f74a67679f7f801da2c9c262ea6
Anna Schaar <anna.schaar@tum.de>
2023-05-09 10:18:40 +0200

    Bump version from 0.1.4 to 0.1.5

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ copyright = "2021, theislab"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.1.4"
+version = "0.1.5"
 # The full version, including alpha/beta/rc tags.
-release = "0.1.4"
+release = "0.1.5"
 
 
 # Add any Sphinx extension module names here, as strings. They can be
