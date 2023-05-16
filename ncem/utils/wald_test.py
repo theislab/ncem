4eceb8df82f8ca1732d2576e4c5c3ad66207dfcb
Anna Schaar <anna.schaar@tum.de>
2023-05-16 13:43:11 +0200

    adjust naming in dmats

--- ncem/utils/wald_test.py
+++ ncem/utils/wald_test.py
@@ -6,7 +6,7 @@ def get_fim_inv(x: np.array, y: np.array):
     if issparse(y):
         y = y.toarray()
     var = np.var(y, axis=0, keepdims=True)
-    fim = np.expand_dims(np.matmul(x.T, x), axis=0) / np.expand_dims(var, axis=[1]).T
+    fim = np.expand_dims(np.matmul(np.array(x.T), np.array(x)), axis=0) / np.expand_dims(var, axis=[1]).T
     fim = np.nan_to_num(fim)
 
     fim_inv = np.array([np.linalg.pinv(fim[i, :, :]) for i in range(fim.shape[0])])
